4bd43d73858f55d5511d7813443d6fbab07d18c5
IlyaKorshunov <korshunov.work@gmail.com>
2023-11-20 13:53:56 +0300

    initial commit

---
+++ task-41/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 41:
+// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+// Функция находит колличество положительных элементов double-массива.
+int CountPosNumbersInArray(double[] array)
+{
+    int result = 0;
+    foreach (double item in array)
+    {
+        if (item > 0) result++;
+    }
+    return result;
+}
+
+// Функция запрашивает ввод строки с числами, разделенными пробелами, и преобразует ее в double-массив.
+double[] InputDoubleArrayFromKeyboard(string message)
+{
+    Console.WriteLine(message);
+    string[] numbersString = Console.ReadLine().Trim().Replace('.', ',').Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+    double[] numbers = new double[numbersString.Length];
+    bool check = false;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        check = double.TryParse(numbersString[i], out numbers[i]);
+        if (check == false)
+        {
+            break;
+        }
+    }
+    while (check == false)
+    {
+        Console.WriteLine($"НЕВЕРНЫЕ ДАННЫЕ. {message}");
+        numbersString = Console.ReadLine().Trim().Replace('.', ',').Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+        numbers = new double[numbersString.Length];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            check = double.TryParse(numbersString[i], out numbers[i]);
+            if (check == false)
+            {
+                break;
+            }
+        }
+    }
+    return numbers;
+}
+
+Console.Clear();
+double[] userArray = InputDoubleArrayFromKeyboard("Введите числа, разделяя их пробелом");
+Console.Clear();
+Console.WriteLine($"Ваш массив: [{String.Join(", ", userArray)}]");
+int posNumbersCount = CountPosNumbersInArray(userArray);
+Console.WriteLine($"Количество положительных чисел: {posNumbersCount}");
---
+++ task-43/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 43:
+// Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// Функция запрашивает ввод double-числа и проверят его на корректность.
+double CheckInputDouble(string message)
+{
+    bool check = false;
+    Console.Write($"{message} ");
+    check = double.TryParse(Console.ReadLine().Trim().Replace('.', ','), out double number);
+    while (check == false)
+    {
+        Console.Write($"Неверные данные. {message} ");
+        check = double.TryParse(Console.ReadLine().Trim().Replace('.', ','), out number);
+    }
+    return number;
+}
+
+// Функция вычисляет точку пересечения графиков уравнений.
+double[] GetIntersecPoint(double k1, double b1, double k2, double b2)
+{
+    double[] intersecPoint = new double[2];
+    intersecPoint[0] = (b2 - b1) / (k1 - k2);
+    intersecPoint[1] = k1 * intersecPoint[0] + b1;
+    return intersecPoint;
+}
+
+double[] coeffArray = new double[4];
+Console.Clear();
+Console.WriteLine("Введите коэффициенты для функции  Y = K1 * X + B1");
+Console.WriteLine("---------------------------------------------------");
+coeffArray[0] = CheckInputDouble("Введите коэффициент K1:");
+coeffArray[1] = CheckInputDouble("Введите коэффициент B1:");
+Console.WriteLine();
+Console.WriteLine("Введите коэффициенты для уравнения  Y = K2 * X + B2");
+Console.WriteLine("---------------------------------------------------");
+coeffArray[2] = CheckInputDouble("Введите коэффициент K2:");
+coeffArray[3] = CheckInputDouble("Введите коэффициент B2:");
+Console.WriteLine();
+if (coeffArray[0] == coeffArray[2])
+{
+    Console.WriteLine("Графики функций не пересекаются");
+}
+else
+{
+    double[] intersecPoint = GetIntersecPoint(coeffArray[0], coeffArray[1], coeffArray[2], coeffArray[3]);
+    Console.WriteLine($"Координаты точки пересечения графиков функций: {intersecPoint[0]:f3} {intersecPoint[1]:f3}");
+}
+Console.WriteLine();
